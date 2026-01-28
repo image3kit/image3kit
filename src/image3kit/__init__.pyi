d14ddaa50c98a012890d7bffc8e418f371db4589
Ali Raeini <a.qaseminejad_raeini@hw.ac.uk>
2026-01-28 14:21:34 +0000

    rename python package from voxlib to image3

--- src/image3kit/__init__.pyi
+++ src/image3kit/__init__.pyi
@@ -11,13 +11,13 @@
     
 """
 from __future__ import annotations
-from voxlib._core import Input
-from voxlib._core import VxlImgU8
-from voxlib._core import cube
-from voxlib._core import cylinder
-from voxlib._core import readImage
-from voxlib._core import readImageU8
-from voxlib._core import sphere
+from image3kit._core import Input
+from image3kit._core import VxlImgU8
+from image3kit._core import cube
+from image3kit._core import cylinder
+from image3kit._core import readImage
+from image3kit._core import readImageU8
+from image3kit._core import sphere
 from . import _core
 __all__: list = ['__doc__', '__version__', 'VxlImgU8', 'Input', 'sphere', 'cylinder', 'cube', 'readImage', 'readImageU8']
 __version__: str = '0.0.1'
--- tests/test_2to3d.py
+++ tests/test_2to3d.py
@@ -2,13 +2,16 @@ from __future__ import annotations
 
 from pathlib import Path
 
-import voxlib as vx
+import image3kit as ik
 
 cwd = Path(__file__).parent
 def test_readPng():
-    img = vx.VxlImgU8()
+    img = ik.VxlImgU8()
     assert img.data().shape == (0,0,0)
-    img = vx.readImage(cwd / "piskelapp.png")
+    img = ik.readImage(cwd / "piskelapp.png")
+    assert img.shape() == img.data().shape
+    assert img.shape() == img.data().shape
+    img.plotAll()
     print(img, img.data().shape)
 
 if __name__ == "__main__":
