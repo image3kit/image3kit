aliraeini/image3d

rename python package from voxlib to image3

Makefile
@@ -1,7 +1,7 @@
 build_update:
 	nox -s build
 	pip install .
-	pybind11-stubgen voxlib --output-dir src
+	pybind11-stubgen image3d --output-dir src
 
 install:
 	pip install .

image3kit/image3kit

Makefile
@@ -1,7 +1,7 @@
 
 all:
 	pip install .
-	pybind11-stubgen voxlib --output-dir src
+	pybind11-stubgen image3kit --output-dir src
 
 install:
 	pip install .