aliraeini/image3d

update (Linux) Makefile

Makefile
@@ -1,5 +1,16 @@
+
+help:
+	@echo "Install prerequisite using commands:"
+	@echo "  python -m venv ./.venv"
+	@echo "  source .venv/bin/activate"
+	@echo "  pip install pybind11-stubgen numpy  pytest"
+	@echo
+	@echo "Available target commands:"
+	@echo "  make build_update # Install and update pybind11 stubs"
+	@echo "  make install      # Install the package"
+	@echo "  make test         # Run tests"
+
 build_update:
-	# nox -s build
 	pip install .
 	pybind11-stubgen image3d --output-dir src
 

image3kit/image3kit

Makefile
@@ -1,4 +1,15 @@
 
+help:
+	@echo "Install prerequisite using commands:"
+	@echo "  python -m venv ./.venv"
+	@echo "  source .venv/bin/activate"
+	@echo "  pip install pybind11-stubgen numpy  pytest"
+	@echo
+	@echo "Available target commands:"
+	@echo "  make all # Install and update pybind11 stubs"
+	@echo "  make install      # Install the package"
+	@echo "  make test         # Run tests"
+
 all:
 	pip install .
 	pybind11-stubgen image3kit --output-dir src