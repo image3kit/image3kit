aliraeini/image3d

mass_commit: refactorings, bugfixes and exposing U16... voxelImage types to Python

tests/test_2to3d.py
@@ -2,19 +2,27 @@ from __future__ import annotations
 
 from pathlib import Path
 
-import image3d as im
+import image3d as c3d
 
 cwd = Path(__file__).parent
+
 def test_readPng():
-    img = im.VxlImgU8()
+    img = c3d.VxlImgU8()
     assert img.data().shape == (0,0,0)
-    img = im.readImage(cwd / "piskelapp.png")
-    assert img.shape() == img.data().shape
-    assert img.shape() == img.data().shape
-    img.plotAll()
+    img = c3d.VxlImgU8(cwd / "piskelapp.png")
     print(img, img.data().shape)
+    img.printInfo()
+    assert img.shape() == img.data().shape
+    assert img.nz() == 1
+    img.plotAll(zProfile=False) # FIXME: zProfile hangs svplot when nz==1
+    img.distMapExtrude(offset=0.5, scale=2.0,)
+    assert img.nz() > 1
+    slice=8
+    img.plotAll(name="extruded", normalAxis="z", sliceIndex=slice)
+    assert Path(f"fig/extruded_z{slice}_grey.png").exists()
 
 if __name__ == "__main__":
     # test_version()
     # test_voxlibI()
     test_readPng()
+ 

image3kit/image3kit

tests/test_2to3d.py
@@ -5,16 +5,24 @@ from pathlib import Path
 import image3kit as ik
 
 cwd = Path(__file__).parent
+
 def test_readPng():
     img = ik.VxlImgU8()
     assert img.data().shape == (0,0,0)
-    img = ik.readImage(cwd / "piskelapp.png")
-    assert img.shape() == img.data().shape
-    assert img.shape() == img.data().shape
-    img.plotAll()
+    img = ik.VxlImgU8(cwd / "piskelapp.png")
     print(img, img.data().shape)
+    img.printInfo()
+    assert img.shape() == img.data().shape
+    assert img.nz() == 1
+    img.plotAll(zProfile=False) # FIXME: zProfile hangs svplot when nz==1
+    img.distMapExtrude(offset=0.5, scale=2.0,)
+    assert img.nz() > 1
+    slice=8
+    img.plotAll(name="extruded", normalAxis="z", sliceIndex=slice)
+    assert Path(f"fig/extruded_z{slice}_grey.png").exists()
 
 if __name__ == "__main__":
     # test_version()
     # test_voxlibI()
     test_readPng()
+ 